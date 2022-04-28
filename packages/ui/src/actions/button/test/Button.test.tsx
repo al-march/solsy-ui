216e493fe04bb1756123b39b8dc3440e972475a8
Alexander <valverin95@gmail.com>
2022-04-28 18:52:21 +0300

    🧪 [ui] style

--- packages/ui/src/actions/button/test/Button.test.tsx
+++ packages/ui/src/actions/button/test/Button.test.tsx
@@ -5,77 +5,77 @@ import { Button } from '../Button';
 describe('Button', () => {
 
     test('should be rendered', async () => {
-        await render(() => <Button>button</Button>);
+        render(() => <Button>button</Button>);
         const component = await screen.findByRole('button');
         expect(component).toBeInTheDocument();
     });
 
     test('should add color classes', async () => {
-        await render(() => <Button color="primary">button primary</Button>);
+        render(() => <Button color="primary">button primary</Button>);
         expect(await screen.findByText('button primary')).toHaveClass('btn-primary');
 
-        await render(() => <Button color="secondary">button secondary</Button>);
+        render(() => <Button color="secondary">button secondary</Button>);
         expect(await screen.findByText('button secondary')).toHaveClass('btn-secondary');
 
-        await render(() => <Button color="accent">button accent</Button>);
+        render(() => <Button color="accent">button accent</Button>);
         expect(await screen.findByText('button accent')).toHaveClass('btn-accent');
 
-        await render(() => <Button color="info">button info</Button>);
+        render(() => <Button color="info">button info</Button>);
         expect(await screen.findByText('button info')).toHaveClass('btn-info');
 
-        await render(() => <Button color="success">button success</Button>);
+        render(() => <Button color="success">button success</Button>);
         expect(await screen.findByText('button success')).toHaveClass('btn-success');
 
-        await render(() => <Button color="warning">button warning</Button>);
+        render(() => <Button color="warning">button warning</Button>);
         expect(await screen.findByText('button warning')).toHaveClass('btn-warning');
 
-        await render(() => <Button color="error">button error</Button>);
+        render(() => <Button color="error">button error</Button>);
         expect(await screen.findByText('button error')).toHaveClass('btn-error');
 
-        await render(() => <Button color="ghost">button ghost</Button>);
+        render(() => <Button color="ghost">button ghost</Button>);
         expect(await screen.findByText('button ghost')).toHaveClass('btn-ghost');
 
-        await render(() => <Button link>button link</Button>);
+        render(() => <Button link>button link</Button>);
         expect(await screen.findByText('button link')).toHaveClass('btn-link');
     });
 
     test('should add type classes', async () => {
-        await render(() => <Button outline>button</Button>);
+        render(() => <Button outline>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('btn-outline');
         cleanup();
 
-        await render(() => <Button active>button</Button>);
+        render(() => <Button active>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('btn-active');
         cleanup();
 
-        await render(() => <Button disabled>button</Button>);
+        render(() => <Button disabled>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('btn-disabled');
         cleanup();
 
-        await render(() => <Button circle>button</Button>);
+        render(() => <Button circle>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('btn-circle');
         cleanup();
 
-        await render(() => <Button square>button</Button>);
+        render(() => <Button square>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('btn-square');
         cleanup();
 
-        await render(() => <Button loading>button</Button>);
+        render(() => <Button loading>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('loading');
         cleanup();
 
-        await render(() => <Button glass>button</Button>);
+        render(() => <Button glass>button</Button>);
         expect(await screen.findByText('button')).toHaveClass('glass');
         cleanup();
     })
 
     test('should add custom classes', async () => {
-        await render(() => <Button class="my-custom-class">button</Button>);
+        render(() => <Button class="my-custom-class">button</Button>);
         expect(await screen.findByText('button')).toHaveClass('my-custom-class')
     });
 
     test('should disable button', async () => {
-        await render(() => <Button disabled>button</Button>);
+        render(() => <Button disabled>button</Button>);
         expect(await screen.findByText('button')).toBeDisabled();
         expect(await screen.findByText('button')).toHaveClass('btn-disabled');
     })
