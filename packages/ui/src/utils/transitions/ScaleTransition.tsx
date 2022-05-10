993a0c5390a0d5947c80b120c25daee76882cdca
Alexander <valverin95@gmail.com>
2022-05-10 13:19:04 +0300

    ⚙️ [ui] transition fixes

--- packages/ui/src/utils/transitions/ScaleTransition.tsx
+++ packages/ui/src/utils/transitions/ScaleTransition.tsx
@@ -1,50 +1,50 @@
 import { Component } from 'solid-js';
 import { Transition } from 'solid-transition-group';
 
-const onEnter = (el: Element) => {
-    return el.animate?.([{
-        opacity: 0,
-        transform: 'scale(0.8) translateX(-5px) translateY(20px)',
-    }, {
-        opacity: 1,
-        transform: 'scale(1) translateX(0) translateY(0)'
-    }], {
-        duration: 160,
-        easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
-    });
-};
-
-const onExit = (el: Element) => {
-    return el.animate?.([{
-        opacity: 1,
-        transform: 'scale(1)'
-    }, {
-        opacity: 0,
-        transform: 'scale(0.90)'
-    }], {
-        duration: 120,
-        easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
-    }).finished || Promise.resolve();
-};
-
 type Props = {
     appear?: boolean;
     onExit?: () => void;
 }
 
 export const ScaleTransition: Component<Props> = (props) => {
-
     const onExitDone = () => {
-        if (props.onExit) {
-            props.onExit();
-        }
+        props.onExit?.();
     };
 
     return (
         <Transition
-            appear={!!props.appear}
-            onEnter={onEnter}
-            onExit={(el) => onExit(el).then(onExitDone)}
+            appear={true}
+            onBeforeEnter={(el) => ((el as HTMLElement).style.opacity = '0')}
+            onEnter={(el, done) => {
+                const a = el.animate?.([{
+                    opacity: 0,
+                    transform: 'scale(0.8) translateX(-5px) translateY(20px)',
+                }, {
+                    opacity: 1,
+                    transform: 'scale(1) translateX(0) translateY(0)'
+                }], {
+                    duration: 160,
+                    easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
+                }).finished || Promise.resolve();
+                a.then(done);
+            }}
+            onAfterEnter={(el) => ((el as HTMLElement).style.opacity = '1')}
+            onExit={(el, done) => {
+                const a = el.animate?.([{
+                    opacity: 1,
+                    transform: 'scale(1)'
+                }, {
+                    opacity: 0,
+                    transform: 'scale(0.90)'
+                }], {
+                    duration: 120,
+                    easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
+                }).finished || Promise.resolve();
+                a.then(() => {
+                    onExitDone();
+                    done();
+                });
+            }}
         >
             {props.children}
         </Transition>
--- packages/ui/src/utils/transitions/SlideUpTransition.tsx
+++ packages/ui/src/utils/transitions/SlideUpTransition.tsx
@@ -1,31 +1,6 @@
 import { Transition } from 'solid-transition-group';
 import { Component } from 'solid-js';
 
-const onEnter = (el: Element) => {
-    return el.animate?.([{
-        opacity: 0,
-        transform: 'scale(0.95) translateY(40px)',
-    }, {
-        opacity: 1,
-        transform: 'scale(1) translateY(0)'
-    }], {
-        duration: 120,
-        easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
-    });
-};
-
-const onExit = (el: Element) => {
-    return el.animate?.([{
-        opacity: 1,
-        transform: 'scale(1) translateY(0)',
-    }, {
-        opacity: 0,
-        transform: 'scale(0.95) translateY(40px)',
-    }], {
-        duration: 120
-    }).finished || Promise.resolve();
-};
-
 type Props = {
     appear?: boolean;
     onExit?: () => void;
@@ -38,9 +13,37 @@ export const SlideUpTransition: Component<Props> = (props) => {
 
     return (
         <Transition
-            appear={!!props.appear}
-            onEnter={onEnter}
-            onExit={(el) => onExit(el).then(onExitDone)}
+            appear={true}
+            onBeforeEnter={(el) => ((el as HTMLElement).style.opacity = '0')}
+            onEnter={(el, done) => {
+                const a = el.animate?.([{
+                    opacity: 0,
+                    transform: 'scale(0.95) translateY(40px)',
+                }, {
+                    opacity: 1,
+                    transform: 'scale(1) translateY(0)'
+                }], {
+                    duration: 120,
+                    easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
+                }).finished || Promise.resolve();
+                a.then(done);
+            }}
+            onAfterEnter={(el) => ((el as HTMLElement).style.opacity = '1')}
+            onExit={(el, done) => {
+                const a = el.animate?.([{
+                    opacity: 1,
+                    transform: 'scale(1) translateY(0)',
+                }, {
+                    opacity: 0,
+                    transform: 'scale(0.95) translateY(40px)',
+                }], {
+                    duration: 120
+                }).finished || Promise.resolve();
+                a.then(() => {
+                    onExitDone();
+                    done();
+                });
+            }}
         >
             {props.children}
         </Transition>
