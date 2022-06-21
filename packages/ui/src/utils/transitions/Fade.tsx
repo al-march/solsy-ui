c68d4267a473a5f03d75be7192aa1c00bbd806dd
Alexander <valverin95@gmail.com>
2022-06-22 01:08:18 +0300

    🔥️ [ui] Fade

---
+++ packages/ui/src/utils/transitions/Fade.tsx
@@ -0,0 +1,44 @@
+import { Component } from 'solid-js';
+import { Transition } from 'solid-transition-group';
+
+type Props = {
+  appear?: boolean;
+  onExit?: () => void;
+  duration?: number;
+}
+
+export const Fade: Component<Props> = (props) => {
+  const onExitDone = () => {
+    props.onExit?.();
+  };
+
+  const keyframes = [{
+    opacity: 0,
+  }, {
+    opacity: 1,
+  }];
+
+  const options = {
+    duration: props.duration,
+    easing: 'cubic-bezier(0.55, 0, 0.55, 0.2)'
+  };
+
+  return (
+    <Transition
+      appear={true}
+      onBeforeEnter={(el) => (el as HTMLElement).style.opacity = '0'}
+      onEnter={async (el, done) => {
+        await el.animate?.(keyframes, options).finished;
+        done();
+      }}
+      onAfterEnter={(el) => ((el as HTMLElement).style.opacity = '1')}
+      onExit={async (el, done) => {
+        await el.animate?.(keyframes.reverse(), options).finished;
+        onExitDone();
+        done();
+      }}
+    >
+      {props.children}
+    </Transition>
+  );
+};
--- packages/ui/src/utils/transitions/index.ts
+++ packages/ui/src/utils/transitions/index.ts
@@ -1,2 +1,3 @@
 export * from './SlideUpTransition';
 export * from './ScaleTransition';
+export * from './Fade';
