c287227f5c0e05ef63444d9f7503250127a8b9a4
LeXik0N <lexik0n1993@yandex.ru>
2022-08-17 23:47:51 +0300

    ✏ [UI-2] added prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,18 @@
 module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  quoteProps: 'consistent',
+  jsxSingleQuote: true,
+  bracketSpacing: false,
+  bracketSameLine: true,
+  arrowParens: 'avoid',
+  endOfLine: 'lf',
   plugins: [
-    require("@trivago/prettier-plugin-sort-imports"),
-    require("prettier-plugin-sort-json"),
-    require("prettier-plugin-packagejson"),
+    require('@trivago/prettier-plugin-sort-imports'),
+    require('prettier-plugin-sort-json'),
+    require('prettier-plugin-packagejson'),
   ],
 };
