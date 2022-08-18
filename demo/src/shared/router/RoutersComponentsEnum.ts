bad8866e3a6d582981d25a6efa4b6cdfd905bf56
Alexander <valverin95@gmail.com>
2022-08-18 12:05:37 +0300

    🔥️ [UI-12] Divider: created new component

--- demo/src/shared/router/RoutersComponentsEnum.ts
+++ demo/src/shared/router/RoutersComponentsEnum.ts
@@ -14,6 +14,7 @@ export enum RoutersComponentsEnum {
   TOOLTIP = 'tooltip',
   POPOVER = 'popover',
   COLLAPSE = 'collapse',
+  DIVIDER = 'divider',
   TOGGLE = 'toggle',
   DIVIDER = 'divider',
 }
