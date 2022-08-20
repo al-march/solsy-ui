7124a62a0c372ce0015b2858c98ac8cc834ef011
LeXik0N <lexik0n1993@yandex.ru>
2022-08-20 22:06:30 +0300

    🔥️ [UI-4] Added toggle page

--- demo/src/shared/router/RoutersComponentsEnum.ts
+++ demo/src/shared/router/RoutersComponentsEnum.ts
@@ -14,6 +14,7 @@ export enum RoutersComponentsEnum {
   TOOLTIP = 'tooltip',
   POPOVER = 'popover',
   COLLAPSE = 'collapse',
+  TOGGLE = 'toggle',
 }
 
 export enum RouterSDKEnum {
--- demo/src/views/pages/form/index.ts
+++ demo/src/views/pages/form/index.ts
@@ -1,3 +1,4 @@
 export * from './select/SelectPage';
 export * from './range/RangePage';
 export * from './datepicker/DatepickerPage';
+export * from './toggle/TogglePage';
