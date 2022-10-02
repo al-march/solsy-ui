2c9154110a208dc99c952116291a78637e46ad50
Alexander <valverin95@gmail.com>
2022-10-02 23:40:30 +0300

    🔥️ [UI-16] - add AlertsContainer - refactor Alert

---
+++ packages/ui/src/data-display/alert/AlertsContainer.tsx
@@ -0,0 +1,142 @@
+import {SlideUpTransition} from '../../utils';
+import {Alert} from './Alert';
+import {AlertType} from './types';
+import {
+  createContext,
+  createMemo,
+  For,
+  JSXElement,
+  ParentProps,
+  useContext,
+} from 'solid-js';
+import {createStore} from 'solid-js/store';
+import {Portal} from 'solid-js/web';
+
+let alertId = 0;
+
+class AlertsStackItem {
+  constructor(
+    public id: number,
+    public message: string,
+    public type?: AlertType,
+    public timeout = 3000,
+    public action?: JSXElement
+  ) {}
+}
+
+export type AlertsStack = Map<number, AlertsStackItem>;
+
+type AlertsState = {
+  alerts: AlertsStack;
+};
+
+type AlertsCtx = {
+  state: AlertsState;
+
+  show: (alert: AlertsStackItem) => void;
+  info: (message: string, timeout?: number, action?: JSXElement) => void;
+  success: (message: string, timeout?: number, action?: JSXElement) => void;
+  warning: (message: string, timeout?: number, action?: JSXElement) => void;
+  error: (message: string, timeout?: number, action?: JSXElement) => void;
+
+  close: (alertId: number) => void;
+};
+
+const AlertsCtx = createContext<AlertsCtx>();
+
+export const useAlerts = () => {
+  const ctx = useContext(AlertsCtx);
+  if (ctx) {
+    return ctx;
+  }
+  throw new Error('No context for Alerts');
+};
+
+export function createAlert(
+  msg: string,
+  type: AlertType,
+  timeout = 3000,
+  action?: JSXElement
+) {
+  return new AlertsStackItem(alertId++, msg, type, timeout, action);
+}
+
+export const AlertsContainer = (props: ParentProps) => {
+  const [state, setState] = createStore<AlertsState>({
+    alerts: new Map(),
+  });
+
+  const stack = createMemo(() => Array.from(state.alerts.values()).reverse());
+
+  function show(alert: AlertsStackItem) {
+    addAlert(alert);
+  }
+
+  function info(message: string, timeout = 3000, action?: JSXElement) {
+    addAlert(createAlert(message, 'info', timeout, action));
+  }
+
+  function success(message: string, timeout = 3000, action?: JSXElement) {
+    addAlert(createAlert(message, 'success', timeout, action));
+  }
+
+  function warning(message: string, timeout = 3000, action?: JSXElement) {
+    addAlert(createAlert(message, 'warning', timeout, action));
+  }
+
+  function error(message: string, timeout = 3000, action?: JSXElement) {
+    addAlert(createAlert(message, 'error', timeout, action));
+  }
+
+  function addAlert(alert: AlertsStackItem) {
+    const alerts = new Map(state.alerts.set(alert.id, alert));
+    setState('alerts', alerts);
+    setAlertTimeout(alert.id, alert.timeout);
+  }
+
+  function close(alertId: number) {
+    const stack = new Map(state.alerts);
+    stack.delete(alertId);
+    setState('alerts', new Map(stack));
+  }
+
+  function setAlertTimeout(alertId: number, timeout: number) {
+    setTimeout(() => {
+      close(alertId);
+    }, timeout);
+  }
+
+  return (
+    <AlertsCtx.Provider
+      value={{
+        state,
+        show,
+        info,
+        success,
+        warning,
+        error,
+        close,
+      }}
+    >
+      {props.children}
+
+      <Portal>
+        <div class="container fixed bottom-2 left-0 right-0 flex flex-col gap-2">
+          <For each={stack()}>
+            {alert => (
+              <SlideUpTransition appear>
+                <Alert
+                  type={alert.type}
+                  action={alert.action}
+                  onClose={() => close(alert.id)}
+                >
+                  {alert.message}
+                </Alert>
+              </SlideUpTransition>
+            )}
+          </For>
+        </div>
+      </Portal>
+    </AlertsCtx.Provider>
+  );
+};
--- packages/ui/src/data-display/alert/index.ts
+++ packages/ui/src/data-display/alert/index.ts
@@ -1,1 +1,3 @@
-export * from './Alert'
+export * from './Alert';
+export * from './AlertsContainer';
+export * from './types';
