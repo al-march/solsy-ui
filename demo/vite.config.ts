a02b30e1f6645677805d019423696b9a370ffe7a
Alexander <valverin95@gmail.com>
2022-10-06 15:44:22 +0300

    🔥️ [UI-19] add CodePreview comp

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,9 +1,18 @@
 import {defineConfig} from 'vite';
+import prismjs from 'vite-plugin-prismjs';
 import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [solidPlugin(), tsconfigPaths()],
+  plugins: [
+    solidPlugin(),
+    tsconfigPaths(),
+    prismjs({
+      languages: ['bash', 'js', 'tsx', 'html', 'css'],
+      plugins: ['copy-to-clipboard', 'line-highlight'],
+      css: false,
+    }),
+  ],
   build: {
     rollupOptions: {
       external: ['solid-js'],
