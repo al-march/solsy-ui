72d0e754d9753c25cdd33b035ef9bc90d9792b41
Alexander <valverin95@gmail.com>
2022-10-08 09:14:47 +0300

    🔥️ [DEMO] BtnGroup: add code snippets

--- demo/src/views/pages/navigation/btn-group/BtnGroupPage.tsx
+++ demo/src/views/pages/navigation/btn-group/BtnGroupPage.tsx
@@ -1,13 +1,43 @@
 import {Page} from '@page/base';
-import {ImportPreview} from '@shared/components';
+import {CodePreview, ImportPreview, TypeLine} from '@shared/components';
+import {Divider, Row} from '@ui/layout';
 import {BtnGroup} from '@ui/navigation';
-import {Component, createSignal} from 'solid-js';
+import {DaisySizes} from '@ui/types';
+import {Component} from 'solid-js';
+
+const btnGroupSnippet = `function onButtonsChange(v: number) {
+  console.log(v);
+}
+
+<BtnGroup onInput={onButtonsChange}>
+  <BtnGroup.Item>1</BtnGroup.Item>
+  <BtnGroup.Item>2</BtnGroup.Item>
+  <BtnGroup.Item defaultChecked>3</BtnGroup.Item>
+</BtnGroup>`;
+
+const btnGroupVerticalSnippet = `<BtnGroup orientation="vertical">
+  <BtnGroup.Item>1</BtnGroup.Item>
+  <BtnGroup.Item>2</BtnGroup.Item>
+  <BtnGroup.Item>3</BtnGroup.Item>
+</BtnGroup>`;
+
+const btnGroupSizeSnippet = `<Row class="gap-2 py-2" orientation="col">
+  <BtnGroup size="lg">
+    <BtnGroup.Item>lg</BtnGroup.Item>
+  </BtnGroup>
+  <BtnGroup size="md">
+    <BtnGroup.Item>md</BtnGroup.Item>
+  </BtnGroup>
+  <BtnGroup size="sm">
+    <BtnGroup.Item>sm</BtnGroup.Item>
+  </BtnGroup>
+  <BtnGroup size="xs">
+    <BtnGroup.Item>xs</BtnGroup.Item>
+  </BtnGroup>
+</Row>`;
 
 export const BtnGroupPage: Component = () => {
-  const [btn, setBtn] = createSignal();
-
-  function onButtonsChange(v: any) {
-    setBtn(v);
+  function onButtonsChange(v: number) {
     console.log(v);
   }
 
@@ -15,23 +45,70 @@ export const BtnGroupPage: Component = () => {
     <Page full class="p-4">
       <br />
       <ImportPreview component="BtnGroup" />
+
+      <Divider />
+
+      <h2 class="text-2xl">Default use</h2>
+      <p>
+        <span class="text-info">BtnGroup</span> component...
+      </p>
       <br />
 
-      <BtnGroup value={btn()} onInput={onButtonsChange} size="sm">
+      <BtnGroup onInput={onButtonsChange}>
         <BtnGroup.Item>1</BtnGroup.Item>
         <BtnGroup.Item>2</BtnGroup.Item>
         <BtnGroup.Item defaultChecked>3</BtnGroup.Item>
       </BtnGroup>
 
-      <span class="divider" />
+      <CodePreview>{btnGroupSnippet}</CodePreview>
 
-      <div class="w-32">
-        <BtnGroup size="sm" orientation="vertical">
-          <BtnGroup.Item>1</BtnGroup.Item>
-          <BtnGroup.Item>2</BtnGroup.Item>
-          <BtnGroup.Item>3</BtnGroup.Item>
+      <Divider />
+
+      <h2 class="text-2xl">Orientation</h2>
+      <p>
+        By <span class="text-info">orientation</span>, the support changes
+        position to position to <br />
+        <span class="text-info">vertical</span> or{' '}
+        <span class="text-info">horizontal</span>
+      </p>
+
+      <br />
+
+      <BtnGroup orientation="vertical">
+        <BtnGroup.Item>1</BtnGroup.Item>
+        <BtnGroup.Item>2</BtnGroup.Item>
+        <BtnGroup.Item>3</BtnGroup.Item>
+      </BtnGroup>
+
+      <CodePreview>{btnGroupVerticalSnippet}</CodePreview>
+
+      <Divider />
+
+      <h2 class="text-2xl">Sizes</h2>
+      <p>
+        Button sizes in <span class="text-info">BtnGroup</span> change depending
+        on the <span class="text-info">size</span> property
+      </p>
+      <p>
+        There are size props: <br /> <TypeLine types={[...DaisySizes]} />
+      </p>
+
+      <Row class="gap-2 py-2" orientation="col">
+        <BtnGroup size="lg">
+          <BtnGroup.Item>lg</BtnGroup.Item>
         </BtnGroup>
-      </div>
+        <BtnGroup size="md">
+          <BtnGroup.Item>md</BtnGroup.Item>
+        </BtnGroup>
+        <BtnGroup size="sm">
+          <BtnGroup.Item>sm</BtnGroup.Item>
+        </BtnGroup>
+        <BtnGroup size="xs">
+          <BtnGroup.Item>xs</BtnGroup.Item>
+        </BtnGroup>
+      </Row>
+
+      <CodePreview>{btnGroupSizeSnippet}</CodePreview>
     </Page>
   );
 };
