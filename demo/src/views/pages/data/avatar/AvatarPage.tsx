8add51a838137866fe010e34b8ca56067c232543
Alexander <valverin95@gmail.com>
2022-10-08 07:55:57 +0300

    🔥️ [DEMO] Avatar: add code snippets

--- demo/src/views/pages/data/avatar/AvatarPage.tsx
+++ demo/src/views/pages/data/avatar/AvatarPage.tsx
@@ -1,6 +1,12 @@
 import {Page} from '@page/base';
-import {ImportPreview} from '@shared/components';
+import {CodePreview, ImportPreview} from '@shared/components';
 import {Avatar} from '@ui/data-display';
+
+const avatarPreview = `<Avatar class="m-4">
+  <Avatar.Item class="w-32 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+    <img src="https://placeimg.com/192/192/people" alt="" />
+  </Avatar.Item>
+</Avatar>`;
 
 export const AvatarPage = () => {
   return (
@@ -16,6 +22,8 @@ export const AvatarPage = () => {
           <img src="https://placeimg.com/192/192/people" alt="" />
         </Avatar.Item>
       </Avatar>
+
+      <CodePreview>{avatarPreview}</CodePreview>
     </Page>
   );
 };
