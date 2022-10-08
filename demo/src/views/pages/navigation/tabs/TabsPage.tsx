f01889020da4664e81363133e7d791dc8189d878
Alexander <valverin95@gmail.com>
2022-10-08 09:21:32 +0300

    🔥️ [DEMO] TabsPage: add code snippets

--- demo/src/views/pages/navigation/tabs/TabsPage.tsx
+++ demo/src/views/pages/navigation/tabs/TabsPage.tsx
@@ -1,7 +1,20 @@
 import {Page} from '@page/base';
-import {ImportPreview} from '@shared/components';
+import {CodePreview, ImportPreview} from '@shared/components';
+import {Divider} from '@ui/layout';
 import {Tabs} from '@ui/navigation';
 import {Component} from 'solid-js';
+
+const tabsSnippet = `<Tabs view="boxed" size="sm">
+  <Tabs.Item label="label 1">Tab 1</Tabs.Item>
+  <Tabs.Item label="label 2">Tab 2</Tabs.Item>
+  <Tabs.Item label="label 3">Tab 3</Tabs.Item>
+</Tabs>`;
+
+const tabsOrientationSnippet = `<Tabs view="boxed" orientation="vertical">
+  <Tabs.Item label="label 1">Tab 1</Tabs.Item>
+  <Tabs.Item label="label 2">Tab 2</Tabs.Item>
+  <Tabs.Item label="label 3">Tab 3</Tabs.Item>
+</Tabs>`;
 
 export const TabsPage: Component = () => {
   function onInput(index: number) {
@@ -12,6 +25,13 @@ export const TabsPage: Component = () => {
     <Page full class="p-4">
       <br />
       <ImportPreview component="Tabs" />
+
+      <Divider />
+
+      <h2 class="text-2xl">Default use</h2>
+      <p>
+        <span class="text-info">Tabs</span> component...
+      </p>
       <br />
 
       <Tabs view="boxed" size="sm" onInput={onInput}>
@@ -19,17 +39,28 @@ export const TabsPage: Component = () => {
         <Tabs.Item label="label 2">Tab 2</Tabs.Item>
         <Tabs.Item label="label 3">Tab 3</Tabs.Item>
       </Tabs>
-      <Tabs
-        view="boxed"
-        size="md"
-        orientation="vertical"
-        class="my-custom-wrapper"
-        onInput={onInput}
-      >
+
+      <CodePreview>{tabsSnippet}</CodePreview>
+
+      <Divider />
+
+      <h2 class="text-2xl">Orientation</h2>
+      <p>
+        By <span class="text-info">orientation</span>, the support changes
+        position to position to <br />
+        <span class="text-info">vertical</span> or{' '}
+        <span class="text-info">horizontal</span>
+      </p>
+
+      <br />
+
+      <Tabs view="boxed" orientation="vertical" onInput={onInput}>
         <Tabs.Item label="label 1">Tab 1</Tabs.Item>
         <Tabs.Item label="label 2">Tab 2</Tabs.Item>
         <Tabs.Item label="label 3">Tab 3</Tabs.Item>
       </Tabs>
+
+      <CodePreview>{tabsOrientationSnippet}</CodePreview>
     </Page>
   );
 };
