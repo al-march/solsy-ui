c7fa8c93029a74db1f64e1dddc2f416e9805e310
Alexander <valverin95@gmail.com>
2022-10-09 13:46:49 +0300

    🔥️️ [UI] Update progress & add tests

--- packages/ui/src/data-display/progress/Progress.tsx
+++ packages/ui/src/data-display/progress/Progress.tsx
@@ -1,17 +1,39 @@
 import {DaisyColor} from '../../types';
-import {ParentProps} from 'solid-js';
+import {Match, mergeProps, ParentProps, Switch} from 'solid-js';
 
-export type ProgressProps = {
-  value: number;
-  max?: number;
-  color?: DaisyColor;
+export const ProgressSelectors = {
+  PROGRESS: 'progress',
 };
 
-export const Progress = (props: ParentProps<ProgressProps>) => {
+export type ProgressProps = {
+  value?: number | string;
+  max?: number;
+  color?: DaisyColor;
+  ref?: (ref: HTMLProgressElement) => void;
+
+  class?: string;
+  id?: string;
+  indeterminate?: boolean;
+};
+
+export const ProgressDefaultProps = {
+  value: 0,
+  max: 100,
+  color: 'primary',
+  class: '',
+};
+
+/* Fixme: cannot set value prop to HTMLProgressElement as undefined */
+const ProgressIndeterminate = (props: ProgressProps) => {
   return (
     <progress
+      data-testid={ProgressSelectors.PROGRESS}
+      id={props.id}
+      ref={props.ref}
       class="progress"
       classList={{
+        [props.class || '']: !!props.class,
+
         'progress-primary': props.color === 'primary',
         'progress-secondary': props.color === 'secondary',
         'progress-accent': props.color === 'accent',
@@ -20,8 +42,38 @@ export const Progress = (props: ParentProps<ProgressProps>) => {
         'progress-warning': props.color === 'warning',
         'progress-error': props.color === 'error',
       }}
-      value={props.value || 0}
-      max={props.max || 100}
     />
+  );
+};
+
+export const Progress = (props: ParentProps<ProgressProps>) => {
+  const pr = mergeProps({...ProgressDefaultProps}, props);
+  return (
+    <Switch>
+      <Match when={pr.indeterminate} keyed>
+        <ProgressIndeterminate {...props} />
+      </Match>
+      <Match when={!pr.indeterminate} keyed>
+        <progress
+          data-testid={ProgressSelectors.PROGRESS}
+          id={pr.id}
+          ref={pr.ref}
+          class="progress"
+          classList={{
+            [pr.class]: !!pr.class,
+
+            'progress-primary': pr.color === 'primary',
+            'progress-secondary': pr.color === 'secondary',
+            'progress-accent': pr.color === 'accent',
+            'progress-info': pr.color === 'info',
+            'progress-success': pr.color === 'success',
+            'progress-warning': pr.color === 'warning',
+            'progress-error': pr.color === 'error',
+          }}
+          value={pr.value}
+          max={pr.max}
+        />
+      </Match>
+    </Switch>
   );
 };
---
+++ packages/ui/src/data-display/progress/test/Progress.test.tsx
@@ -0,0 +1,41 @@
+import {DaisyColors} from '../../../types';
+import {Progress, ProgressDefaultProps, ProgressSelectors} from '../Progress';
+import {cleanup, render, screen} from 'solid-testing-library';
+
+const getProgress = () =>
+  screen.getByTestId(ProgressSelectors.PROGRESS) as HTMLProgressElement;
+
+describe('Progress', () => {
+  test('should be rendered', () => {
+    render(() => <Progress />);
+    expect(getProgress()).toBeInTheDocument();
+  });
+  test('should set colors', () => {
+    DaisyColors.forEach(color => {
+      render(() => <Progress color={color} />);
+      expect(getProgress()).toHaveClass(`progress-${color}`);
+      cleanup();
+    });
+  });
+  test('should set custom classes', () => {
+    const className = 'custom-class';
+    render(() => <Progress class={className} />);
+    expect(getProgress()).toHaveClass(className);
+  });
+  test('should set id', () => {
+    const id = 'progress-id';
+    render(() => <Progress id={id} />);
+    expect(getProgress()).toHaveAttribute('id', id);
+  });
+  test('should set default props', () => {
+    render(() => <Progress />);
+    expect(getProgress().value).toBe(ProgressDefaultProps.value);
+    expect(getProgress().max).toBe(ProgressDefaultProps.max);
+  });
+  test('should extract ref', () => {
+    let ref: HTMLProgressElement | undefined;
+    render(() => <Progress ref={r => (ref = r)} />);
+    expect(ref).toBeTruthy();
+    expect(ref instanceof HTMLProgressElement).toBeTruthy();
+  });
+});
