00756be950d2e8dd728864bd708dccf2de8bd0b7
Alexander <valverin95@gmail.com>
2022-10-15 20:21:30 +0300

    ✏️️️️ [DEMO] modalPage refactor

--- demo/src/views/pages/actions/modal/ModalPage.tsx
+++ demo/src/views/pages/actions/modal/ModalPage.tsx
@@ -1,17 +1,8 @@
-import {Code, ImportPreview} from '@shared/components';
+import {DefaultExample} from './examples/DefaultExample';
+import {ImportPreview} from '@shared/components';
 import {Page} from '@shared/components/page';
-import {Button, Modal, ModalAction} from '@ui/actions';
-import {Divider} from '@ui/layout';
-import {Component, createSignal} from 'solid-js';
 
-export const ModalPage: Component = () => {
-  const [show, setShow] = createSignal(false);
-  let closeButton;
-
-  function toggleModal() {
-    setShow(!show());
-  }
-
+export const ModalPage = () => {
   return (
     <Page full class="p-4">
       <h2 class="text-2xl">Modals</h2>
@@ -25,50 +16,7 @@ export const ModalPage: Component = () => {
       <Page.Section name={'default usage'}>
         <h2 class="text-2xl">Default usage</h2>
         <br />
-        <Button
-          ref={ref => (closeButton = ref)}
-          color="primary"
-          onClick={toggleModal}
-        >
-          Open
-        </Button>
-
-        <Code>{`
-          export const ModalExample = () => {
-            const [show, setShow] = createSignal(false);
-            const toggleModal = () => setShow(!show());
-          
-            return (
-              <>
-                <Button color="primary" onClick={toggleModal}>
-                  Open
-                </Button>
-          
-                <Modal isShow={show()} onBackdropClick={toggleModal}>
-                  <h3 class="font-bold text-2xl">Modal title</h3>
-                  <Divider />
-                  <p>Modal description</p>
-                  <ModalAction>
-                    <Button onClick={toggleModal}>Yay!</Button>
-                  </ModalAction>
-                </Modal>
-              </>
-            );
-          };
-        `}</Code>
-
-        <Modal
-          isShow={show()}
-          onBackdropClick={toggleModal}
-          trigger={closeButton}
-        >
-          <h3 class="font-bold text-2xl">Modal title</h3>
-          <Divider />
-          <p>Modal description</p>
-          <ModalAction>
-            <Button onClick={toggleModal}>Yay!</Button>
-          </ModalAction>
-        </Modal>
+        <DefaultExample />
       </Page.Section>
     </Page>
   );
