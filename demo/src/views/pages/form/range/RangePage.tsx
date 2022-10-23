f3f08b51bf945264a4d8c585c0eb8ac2d5e73829
Alexander <valverin95@gmail.com>
2022-10-24 00:54:17 +0300

    🔥️️️️️ [DEMO] update RangePage

--- demo/src/views/pages/form/range/RangePage.tsx
+++ demo/src/views/pages/form/range/RangePage.tsx
@@ -9,45 +9,75 @@ export const RangePage: Component = () => {
 
   return (
     <Page full class="p-4">
-      <h2 class="text-2xl">Range</h2>
-
       <Page.Section name="import">
         <ImportPreview component="Range" />
       </Page.Section>
 
       <Page.Section name="usage">
-        <div class="w-96">
-          <Input
-            type="text"
-            placeholder="Set value"
-            bordered
-            value={value()}
-            onInput={e =>
-              setValue(Number((e.target as HTMLInputElement).value))
-            }
-          />
-          <p class="py-4">Value: {value()}</p>
-          <Range value={value()} onInput={setValue} color="primary" />
+        <Page.Title>Usage</Page.Title>
 
-          <br />
+        <Page.Component
+          preview={
+            <div class="w-96">
+              <Input
+                type="text"
+                placeholder="Set value"
+                bordered
+                value={value()}
+                onInput={e => setValue(Number(e.currentTarget.value))}
+              />
+              <p class="py-4">Value: {value()}</p>
+              <Range value={value()} onInput={setValue} color="primary" />
 
-          <Input
-            type="text"
-            placeholder="Set value"
-            bordered
-            value={range()}
-            onInput={e =>
-              setRange(Number((e.target as HTMLInputElement).value))
-            }
-          />
-          <p class="py-4">Range: {range()}</p>
-          <Range
-            value={value()}
-            step={range()}
-            onInput={setValue}
-            color="accent"
-          />
-        </div>
+              <br />
+
+              <Input
+                type="text"
+                placeholder="Set value"
+                bordered
+                value={range()}
+                onInput={e => setRange(Number(e.currentTarget.value))}
+              />
+              <p class="py-4">Range: {range()}</p>
+              <Range
+                value={value()}
+                step={range()}
+                onInput={setValue}
+                color="accent"
+              />
+            </div>
+          }
+          snippet={`
+            <div class="w-96">
+              <Input
+                type="text"
+                placeholder="Set value"
+                bordered
+                value={range()}
+                onInput={e => setRange(Number(e.currentTarget.value))}
+              />
+              <p class="py-4">Value: {value()}</p>
+              <Range value={value()} onInput={setValue} color="primary" />
+
+              <br />
+
+              <Input
+                type="text"
+                placeholder="Set value"
+                bordered
+                value={range()}
+                onInput={e => setRange(Number(e.currentTarget.value))}
+              />
+              <p class="py-4">Range: {range()}</p>
+              <Range
+                value={value()}
+                step={range()}
+                onInput={setValue}
+                color="accent"
+              />
+            </div>
+          `}
+        />
       </Page.Section>
     </Page>
   );
